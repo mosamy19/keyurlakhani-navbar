74e9c8573d6929c790d85b481afbe76742606c7f
samy <mosamy19@gmail.com>
2019-03-25 00:31:24 +0200

    ts array add dropdown property

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,31 +5,31 @@ import {
   ViewChild,
   HostListener,
   OnDestroy
-} from '@angular/core';
-import LINKS from '../../common/global';
+} from "@angular/core";
+import LINKS from "../../common/global";
 // import LINKS from '../../common/global';
-import { WindowWidthService } from '../../services/window-width.service';
-import { Subscription } from 'rxjs';
+import { WindowWidthService } from "../../services/window-width.service";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit, OnDestroy {
   private resizeSubscription: Subscription;
   screenWidth = window.innerWidth;
   links = LINKS;
-  constructor(private windowWidthService: WindowWidthService) { }
+  constructor(private windowWidthService: WindowWidthService) {}
 
-  @ViewChild('header', { read: ElementRef }) header: ElementRef;
+  @ViewChild("header", { read: ElementRef }) header: ElementRef;
 
   ngOnInit() {
     this.resizeSubscription = this.windowWidthService.onResize$.subscribe(
       window => this.onResize(window.innerWidth)
     );
-    this.links.forEach(element => {
-      element.dropdown = false;
+    this.links.map(element => {
+      element["dropdown"] = false;
     });
   }
 
@@ -42,19 +42,19 @@ export class NavbarComponent implements OnInit, OnDestroy {
   onResize(size) {
     this.screenWidth = size;
     if (this.screenWidth > 767) {
-      this.header.nativeElement.classList.remove('menu-opened');
+      this.header.nativeElement.classList.remove("menu-opened");
     }
   }
 
   handleMenuClick(e) {
     const screenWidth = window.innerWidth;
     if (screenWidth < 768) {
-      this.header.nativeElement.classList.toggle('menu-opened');
+      this.header.nativeElement.classList.toggle("menu-opened");
     }
   }
 
   dropMenu(e, link) {
     link.dropdown ? (link.dropdown = false) : (link.dropdown = true);
-    e.target.closest('.plus').classList.toggle('clicked');
+    e.target.closest(".plus").classList.toggle("clicked");
   }
 }
